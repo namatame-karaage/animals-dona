e44edd87b9f7221c53dd621d42aeed6b089b0d57
namatame-karaage <mariypuuu0519@gmail.com>
2020-09-17 15:21:06 +0900

    signup login page

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'e857307d773e0708df9030ec5af7820d0c3dc5c6ee6917c52c1e472c6218ccb3acfe0e0ca7ae054125d446875d2aa7aa9ec8a179804b3d421a3fd69cb60afd2f'
