4dd5011f391cc38f1e3b445f72b6fc11d20bcaf5
namatame-karaage <mariypuuu0519@gmail.com>
2020-10-25 12:57:01 +0900

    update/js-topslide

---
+++ app/assets/javascripts/scroll.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const el = document.querySelector('.domination__text');
+  const el2 = document.querySelector('#slide-x');
+  // const el3 = document.querySelector('.post-index__top-title');
+  // const el4 = document.querySelector('#after-login');
+  const cb =  (entries, observe) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('scroll')
+        observer.unobserve(entry.target)
+      } else {
+        entry.target.classList.remove('scroll')
+      }
+    })
+  }
+  const options = {
+    root: null,
+    rootMargin: "0px 0px -30px 0px "
+  }
+  const io = new IntersectionObserver(cb, options);
+  io.observe(el);
+  io.observe(el2);
+  io.observe(el3);
+  io.observe(el4);
+})
